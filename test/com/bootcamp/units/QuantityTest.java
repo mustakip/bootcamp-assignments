d63c07e73485205edf6115853b67bf2df2a0646c
mustakim patvekar <44019216+mustakip@users.noreply.github.com>
2019-03-22 09:30:33 +0530

    Made indentation to 2 spaces

--- test/com/bootcamp/units/QuantityTest.java
+++ test/com/bootcamp/units/QuantityTest.java
@@ -5,65 +5,66 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class QuantityTest {
 
-    @Test
-    void ShouldReturnTrueIfBotheQuantityAreEqual() {
-        Quantity feet = new Quantity(new BigDecimal(1), Unit.FEET);
-        Quantity inch = new Quantity(new BigDecimal(12), Unit.INCH);
-        assertEquals(feet,inch);
+  @Test
+  void ShouldReturnTrueIfBotheQuantityAreEqual() {
+    Quantity feet = new Quantity(new BigDecimal(1), Unit.FEET);
+    Quantity inch = new Quantity(new BigDecimal(12), Unit.INCH);
+    assertEquals(feet, inch);
 
-    }
+  }
 
-    @Test
-    void ShouldReturnFalseIfBotheQuantityAreNotEqual() {
-        Quantity feet = new Quantity(new BigDecimal(1), Unit.FEET);
-        Quantity inch = new Quantity(new BigDecimal(1), Unit.INCH);
-        assertNotEquals(feet,inch);
-    }
+  @Test
+  void ShouldReturnFalseIfBotheQuantityAreNotEqual() {
+    Quantity feet = new Quantity(new BigDecimal(1), Unit.FEET);
+    Quantity inch = new Quantity(new BigDecimal(1), Unit.INCH);
+    assertNotEquals(feet, inch);
+  }
 
-    @Test
-    void ShouldReturnTrueIfTwoInchAndFiveCentimetrAreEqual() {
-        Quantity feet = new Quantity(new BigDecimal(2), Unit.INCH);
-        Quantity inch = new Quantity(new BigDecimal(5), Unit.CENTIMETER);
-        assertEquals(feet,inch);
-    }
+  @Test
+  void ShouldReturnTrueIfTwoInchAndFiveCentimetrAreEqual() {
+    Quantity feet = new Quantity(new BigDecimal(2), Unit.INCH);
+    Quantity inch = new Quantity(new BigDecimal(5), Unit.CENTIMETER);
+    assertEquals(feet, inch);
+  }
 
-    @Test
-    void ShouldReturnTrueIfOneCMIsEqualToTENMM() {
-        Quantity centimeter = new Quantity(new BigDecimal(1), Unit.CENTIMETER);
-        Quantity milimeter = new Quantity(new BigDecimal(10), Unit.MILLIMETER);
-        assertEquals(centimeter,milimeter);
-    }
+  @Test
+  void ShouldReturnTrueIfOneCMIsEqualToTENMM() {
+    Quantity centimeter = new Quantity(new BigDecimal(1), Unit.CENTIMETER);
+    Quantity milimeter = new Quantity(new BigDecimal(10), Unit.MILLIMETER);
+    assertEquals(centimeter, milimeter);
+  }
 
-    @Test
-    @DisplayName("3.78 litre should equal to 1 gallon")
-    void ShouldReturnTrue() {
-        Quantity litre = new Quantity(new BigDecimal(3.78), Unit.LITRE);
-        Quantity gallon = new Quantity(new BigDecimal(1), Unit.GALLON);
+  @Test
+  @DisplayName ("3.78 litre should equal to 1 gallon")
+  void ShouldReturnTrue() {
+    Quantity litre = new Quantity(new BigDecimal(3.78), Unit.LITRE);
+    Quantity gallon = new Quantity(new BigDecimal(1), Unit.GALLON);
 
-        assertEquals(litre, gallon);
-    }
+    assertEquals(litre, gallon);
+  }
 
-    @Test
-    @DisplayName("3.78 litre should not equal to 2 gallon")
-    void ShouldReturnFalse() {
-        Quantity litre = new Quantity(new BigDecimal(3.78), Unit.LITRE);
-        Quantity gallon = new Quantity(new BigDecimal(2), Unit.GALLON);
+  @Test
+  @DisplayName ("3.78 litre should not equal to 2 gallon")
+  void ShouldReturnFalse() {
+    Quantity litre = new Quantity(new BigDecimal(3.78), Unit.LITRE);
+    Quantity gallon = new Quantity(new BigDecimal(2), Unit.GALLON);
 
-        assertNotEquals(litre, gallon);
-    }
+    assertNotEquals(litre, gallon);
+  }
 
 
-    @Test
-    @DisplayName("Should not compare units of different type")
-    void test1() {
-        Quantity litre = new Quantity(new BigDecimal(1), Unit.LITRE);
-        Quantity inch = new Quantity(new BigDecimal(1), Unit.INCH);
+  @Test
+  @DisplayName ("Should not compare units of different type")
+  void test1() {
+    Quantity litre = new Quantity(new BigDecimal(1), Unit.LITRE);
+    Quantity inch = new Quantity(new BigDecimal(1), Unit.INCH);
 
-        assertNotEquals(litre, inch);
-    }
+    assertNotEquals(litre, inch);
+  }
 
 }
